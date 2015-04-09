3470f06722949286aaa83028e487563953ac36e4
Abner Yang <abtyang@gmail.com>
2015-04-09 09:18:53 -0400

    w04d03hw

--- server.js
+++ server.js
@@ -1,105 +1,100 @@
 var express = require("express");
 var app = express();
-
 var ejs = require("ejs")
 app.set("view_engine", "ejs")
-
 var override = require("method-override");
 app.use(override("_method"));
-
 var request = require("request");
-
-var fs = require("fs");
-
-var query = {};
-var counting=0;
-
-function delay(func){
-  if(query==={}){
-    console.log("hi"+counting);
-    setTimeout(delay, 100);
-    counting++;
-  }else{
-    console.log(query);
-    func();
-  }
-}
-
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({
-  extended: false
+	extended: false
 }));
+var fs = require("fs");
+
+
+var counter = 0;
+
+// object to hold all movie objects
+var allMovies = {};
+
 
 var port = 3000;
-var counter = 0;
 var Movie = function(title, year, rating, director, actors, plot, poster) {
-  this.id = counter;
-  counter++;
-  this.title = title;
-  this.year = year;
-  this.rating = rating;
-  this.director = director;
-  this.actors = actors;
-  this.plot = plot;
-  this.poster = poster;
+	this.id = counter;
+	counter++;
+	this.title = title;
+	this.year = year;
+	this.rating = rating;
+	this.director = director;
+	this.actors = actors;
+	this.plot = plot;
+	this.poster = poster;
 };
 
 
-var sr = new Movie("random", 2015, "R", " director: us", ["them", "them2", "them3"], "in a world of code...", "http://img2.wikia.nocookie.net/__cb20121110171617/muppet/images/c/c1/MuppetsOfSesameStreet.jpg");
+var sample = new Movie("random", 2015, "R", " director: us", ["them", "them2", "them3"], "in a world of code...", "http://img2.wikia.nocookie.net/__cb20121110171617/muppet/images/c/c1/MuppetsOfSesameStreet.jpg");
 var movies = {
-  0: sr
+	0: sample
 };
 
 app.get("/", function(req, res) {
-  res.redirect("/movies");
+	res.redirect("/movies");
 });
 
 app.get("/movies", function(req, res) {
-  res.render("index.ejs", {
-    movies: movies,
-  });
+	res.render("index.ejs", {
+		movies: movies,
+	});
 });
 
 app.get("/search", function(req, res) {
-  res.render("search.ejs");
+	var term = req.body.title;
+	if (term === undefined) {
+		res.render("search.ejs");
+	} else {
+		res.redirect("/results" + term)
+	}
 });
 
-app.put("/search", function(req, res) {
-  var query = "http://www.omdbapi.com/?t=" + req.body.title + "&y=&plot=short&r=json";
-  request(query, function(err, titleres, titlebod) {
+app.get("/results?:term", function(req, res) {
+	var omdbString = "http://omdbapi.com/?t=" + req.query.title + "&y=&plot=shorts&r=json";
+	request(omdbString, function(error, response, body) {
 
-    var omdb = JSON.parse(titlebod);
-    console.log(omdb);
-    query["result"] = omdb;
+		var parsedOMDB = JSON.parse(body)
 
-  });
-  res.redirect("/search/results");
+		res.render("results.ejs", {
+			parsedOMDB:omdb
+		});
+	});
 });
-console.log("QUERY RESULT CONSOLE: "+query.Title);
-app.get("/search/results", function(req, res) {
-  function happen(){ 
-    res.render("results.ejs", {
-    omdb: query.result,
-  });
-    console.log(query);
-  };
 
-  delay(happen);
-});
+// app.get("/search", function(req, res) {
+//   var query = "http://www.omdbapi.com/?t=" + req.body.title + "&y=&plot=short&r=json";
+//   console.log(req)
+//   request(query, function(err, titleres, titlebod) {
+
+//     var omdb = JSON.parse(titlebod);
+//     console.log(omdb);
+//     query["result"] = omdb;
+
+//   });
+//   res.redirect("/search/results");
+// });
+
 
 app.get("/movie/:id", function(req, res) {
-  var movie = movies[parseInt(req.params.id, 10)];
-  console.log(movie);
-  res.render("show.ejs", {
-    movie: movie
-  });
+	var movie = movies[parseInt(req.params.id, 10)];
+	console.log(movie);
+	res.render("show.ejs", {
+		movie: movie
+	});
 });
 
 app.delete("/movie/:id", function(req, res) {
-  delete movies[parseInt(req.params.id, 10)];
-  res.redirect("/movies");
+	delete movies[parseInt(req.params.id, 10)];
+	res.redirect("/movies");
 });
 
 app.listen(port, function() {
-  console.log("Server is now listening on PORT: " + port);
+	console.log("Server is now listening on PORT: " + port);
 });
